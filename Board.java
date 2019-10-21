e0c9170ee0925c7e72e70541e4cad63d979a5cbd
username2112 <36423664+username2112@users.noreply.github.com>
2019-10-21 14:55:13 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,566 +1,74 @@
 package SpaceInvader;
 
- 
+import java.awt.Image;
 
-import java.awt.Color;
+public class Sprite {
 
-import java.awt.Dimension;
+    private boolean visible;
+    private Image image;
+    protected int x;
+    protected int y;
+    protected boolean dying;
+    protected int dx;
+    protected int dy;
 
-import java.awt.Font;
-
-import java.awt.FontMetrics;
-
-import java.awt.Graphics;
-
-import java.awt.Toolkit;
-
-import java.awt.event.KeyAdapter;
-
-import java.awt.event.KeyEvent;
-
- 
-
-import java.util.ArrayList;
-
-import java.util.Iterator;
-
-import java.util.Random;
-
- 
-
-import javax.swing.ImageIcon;
-
-import javax.swing.JOptionPane;
-
-import javax.swing.JPanel;
-
- 
-
-public class Board extends JPanel implements Runnable, Commons {
-
- 
-
-    private Dimension d;
-
-    private ArrayList<Alien> aliens;
-    private Player player;
-    private Shot shot;
-    //TODO
-    private Boss boss;
+    public Sprite() {
     
-    private int level = 1;
+        visible = true;
+    }
 
-    private final int BOSS_INIT_X = 150;
-    private final int BOSS_INIT_Y = 5;
-    private final int ALIEN_INIT_X = 150;
-    private final int ALIEN_INIT_Y = 5;
-    private int direction = -1;
+    public void die() {
     
-    private int deaths = 0;
-    private int aliencount = 0;
-    private boolean ingame = true;
+        visible = false;
+    }
+
+    public boolean isVisible() {
     
-    private final String explImg = "src/images/explosion.png";
-    private String message = "Game Over";
+        return visible;
+    }
+
+    protected void setVisible(boolean visible) {
     
-    private Thread animator;
+        this.visible = visible;
+    }
+
+    public void setImage(Image image) {
     
-    public Board() {
-        initBoard();
-    }
-    private void initBoard() {
-        addKeyListener(new TAdapter());
-        setFocusable(true);
-        d = new Dimension(BOARD_WIDTH, BOARD_HEIGHT);
-        setBackground(Color.black);
-        gameInit();
-        setDoubleBuffered(true);
-    }
-    @Override
-    public void addNotify() {
-        super.addNotify();
-        gameInit();
+        this.image = image;
     }
 
-    public void gameInit() {      
-        aliens = new ArrayList<>();
-        for (int i = 0; i <= 1 * level; i++) {
-               aliencount++;
-            for (int j = 0; j <= 1 * level; j++) {
-                Alien alien = new Alien(ALIEN_INIT_X + 18 * j, ALIEN_INIT_Y + 18 * i);
-                aliens.add(alien);
-                System.out.println(aliencount);
-            }
-        }
-        if(level > 1)
-        {
-               aliencount *= aliencount;
-               System.out.println(aliencount);
-        }
-        player = new Player();
-        shot = new Shot();
-        boss = new Boss(BOSS_INIT_X, BOSS_INIT_Y);
-        
-        if (animator == null || !ingame) {
-            animator = new Thread(this);
-            animator.start();
-        }
-    }
-public void drawBoss(Graphics g) {
- //TODO
-        if (boss.isVisible()) {
-            g.drawImage(boss.getImage(), boss.getX(), boss.getY(), this);
-        }
-        if (boss.isDying()) {
-            boss.die();
-            ingame = false;
-        }
+    public Image getImage() {
+    
+        return image;
     }
 
-    	
-    public void drawAliens(Graphics g) {
-        Iterator it = aliens.iterator();
-        for (Alien alien: aliens) {
-            if (alien.isVisible()) {
-                g.drawImage(alien.getImage(), alien.getX(), alien.getY(), this);
-            }
-            if (alien.isDying()) {
-                alien.die();
-            }
-        }
+    public void setX(int x) {
+    
+        this.x = x;
     }
 
- 
-
-    public void drawPlayer(Graphics g) {
-        if (player.isVisible()) {
-            g.drawImage(player.getImage(), player.getX(), player.getY(), this);
-
-        }
-        if (player.isDying()) {
-            player.die();
-            ingame = false;
-        }
-    }
- 
-
-    public void drawShot(Graphics g) {
-        if (shot.isVisible()) {
-            g.drawImage(shot.getImage(), shot.getX(), shot.getY(), this);
-        }
-    }
- 
-
-    public void drawBombing(Graphics g) {
-        for (Alien a : aliens) {
-            Alien.Bomb b = a.getBomb();
-            if (!b.isDestroyed()) {
-                g.drawImage(b.getImage(), b.getX(), b.getY(), this);
-            }
-        }
+    public void setY(int y) {
+    
+        this.y = y;
     }
 
-    @Override
-
-    public void paintComponent(Graphics g) {
-
-        super.paintComponent(g);
-
- 
-
-        g.setColor(Color.black);
-
-        g.fillRect(0, 0, d.width, d.height);
-
-        g.setColor(Color.green);
-
- 
-
-        if (ingame) {
-            g.drawLine(0, GROUND, BOARD_WIDTH, GROUND);
-            
-            //TODO draw
-            drawAliens(g);
-            drawPlayer(g);
-            drawShot(g);
-            drawBombing(g);
-            drawBoss(g);
-
-        }
-        Toolkit.getDefaultToolkit().sync();
-        g.dispose();
+    public int getY() {
+    
+        return y;
     }
 
- 
-
-    public void gameOver() {
-
- 
-
-        Graphics g = this.getGraphics();
-
- 
-
-        g.setColor(Color.black);
-
-        g.fillRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
-
-        g.setColor(new Color(0, 32, 48));
-
-        g.fillRect(50, BOARD_WIDTH / 2 - 30, BOARD_WIDTH - 100, 50);
-
-        g.setColor(Color.white);
-
-        g.drawRect(50, BOARD_WIDTH / 2 - 30, BOARD_WIDTH - 100, 50);
-
- 
-
-        Font small = new Font("Helvetica", Font.BOLD, 14);
-
-        FontMetrics metr = this.getFontMetrics(small);
-
- 
-
-        g.setColor(Color.white);
-
-        g.setFont(small);
-
-        g.drawString(message, (BOARD_WIDTH - metr.stringWidth(message)) / 2,
-
-                BOARD_WIDTH / 2);
-
+    public int getX() {
+    
+        return x;
     }
 
-    public void checkIfLevelComplete()
-
-    {
-
-               if(aliencount == 0)
-               {
-                              level++;
-                     // Graphics g = this.getGraphics();
-                     //  g.setColor(Color.black);
-                       //g.fillRect(0, 0, BOARD_WIDTH, BOARD_HEIGHT);
-                       //g.setColor(new Color(0, 32, 48));
-                       //g.fillRect(50, BOARD_WIDTH / 2 - 30, BOARD_WIDTH - 100, 50);
-                       //g.setColor(Color.white);
-                       //g.drawRect(50, BOARD_WIDTH / 2 - 30, BOARD_WIDTH - 100, 50);
-                       //Font small = new Font("Helvetica", Font.BOLD, 14);
-                       //FontMetrics metr = this.getFontMetrics(small);
-                       //g.setColor(Color.white);
-                       //g.setFont(small);
-                       //g.drawString(message, (BOARD_WIDTH - metr.stringWidth(message)) / 2, BOARD_WIDTH / 2);
-                              if(level < 4)
-                              {
-                                             JOptionPane.showMessageDialog(null, "Level Completed");
-                              }
-                              gameInit();
-               }
+    public void setDying(boolean dying) {
+    
+        this.dying = dying;
     }
 
-    public void animationCycle() {
-        if (deaths == NUMBER_OF_ALIENS_TO_DESTROY) {
-            ingame = false;
-            message = "Game won!";
-        }
-        // player
-
-        player.act();
-
-        //boss
-        
-        boss.act();
-
-        // shot
-
-        if (shot.isVisible()) {
-            int shotX = shot.getX();
-            int shotY = shot.getY();
-
-            for (Alien alien: aliens) {
-                int alienX = alien.getX();
-                int alienY = alien.getY();
-
-                if (alien.isVisible() && shot.isVisible()) {
-
-                    if (shotX >= (alienX)
-                            && shotX <= (alienX + ALIEN_WIDTH)
-                            && shotY >= (alienY) &&  shotY <= (alienY + ALIEN_HEIGHT)) {
-
-                        ImageIcon ii = new ImageIcon(explImg);
-                        alien.setImage(ii.getImage());
-                        alien.setDying(true);
-                        deaths++;
-                        shot.die();
-                        aliencount--;
-                    }
-                }
-            }
-
-            int y = shot.getY();
-            y -= 7;
-            
-            if (y < 0) {
-                shot.die();
-            } else {
-                shot.setY(y);
-            }
-        }
-        // aliens
-
- 
-
-        for (Alien alien: aliens) {
-            int x = alien.getX();
-            if (x >= BOARD_WIDTH - BORDER_RIGHT && direction != -1) {
-                direction = -1;
-                Iterator i1 = aliens.iterator();
-                while (i1.hasNext()) {
-                    Alien a2 = (Alien) i1.next();
-                    a2.setY(a2.getY());
-                }
-            }
-            if (x <= BORDER_LEFT && direction != 1) {
-                direction = 1;
-                Iterator i2 = aliens.iterator();
-                while (i2.hasNext()) {
-                    Alien a = (Alien) i2.next();
-                    a.setY(a.getY());
-                }
-            }
-        }
- 
-
-        Iterator it = aliens.iterator();
-        while (it.hasNext()) {
-            Alien alien = (Alien) it.next();
-            if (alien.isVisible()) {
-                int y = alien.getY();
-                if (y > GROUND - ALIEN_HEIGHT) {
-                    ingame = false;
-
-                    message = "Invasion!";
-
-                }
-
- 
-
-                alien.act(direction);
-
-            }
-
-        }
-
- 
-
-        // bombs
-
-        Random generator = new Random();
-
- 
-
-        for (Alien alien: aliens) {
-
- 
-
-            int shot = generator.nextInt(15);
-
-            Alien.Bomb b = alien.getBomb();
-
- 
-
-            if (shot == CHANCE && alien.isVisible() && b.isDestroyed()) {
-
- 
-
-                b.setDestroyed(false);
-
-               b.setX(alien.getX());
-
-                b.setY(alien.getY());
-
-            }
-
- 
-
-            int bombX = b.getX();
-
-            int bombY = b.getY();
-
-            int playerX = player.getX();
-
-            int playerY = player.getY();
-
- 
-
-            if (player.isVisible() && !b.isDestroyed()) {
-
- 
-
-                if (bombX >= (playerX)
-
-                        && bombX <= (playerX + PLAYER_WIDTH)
-
-                        && bombY >= (playerY)
-
-                        && bombY <= (playerY + PLAYER_HEIGHT)) {
-
-                    ImageIcon ii
-
-                            = new ImageIcon(explImg);
-
-                    player.setImage(ii.getImage());
-
-                    player.setDying(true);
-
-                    b.setDestroyed(true);
-
-                }
-
-            }
-
- 
-
-            if (!b.isDestroyed()) {
-
-               
-
-                b.setY(b.getY() + 1);
-
-               
-
-                if (b.getY() >= GROUND - BOMB_HEIGHT) {
-
-                    b.setDestroyed(true);
-
-                }
-
-            }
-
-        }
-
+    public boolean isDying() {
+    
+        return this.dying;
     }
-
- 
-
-    @Override
-
-    public void run() {
-
- 
-
-        long beforeTime, timeDiff, sleep;
-
- 
-
-        beforeTime = System.currentTimeMillis();
-
- 
-
-        while (ingame) {
-
-               checkIfLevelComplete();
-
-            repaint();
-
-            animationCycle();
-
- 
-
-            timeDiff = System.currentTimeMillis() - beforeTime;
-
-            sleep = DELAY - timeDiff;
-
- 
-
-            if (sleep < 0) {
-
-                sleep = 2;
-
-            }
-
- 
-
-            try {
-
-                Thread.sleep(sleep);
-
-            } catch (InterruptedException e) {
-
-                System.out.println("interrupted");
-
-            }
-
-           
-
-            beforeTime = System.currentTimeMillis();
-
-        }
-
- 
-
-        gameOver();
-
-    }
-
- 
-
-    private class TAdapter extends KeyAdapter {
-
- 
-
-        @Override
-
-        public void keyReleased(KeyEvent e) {
-
- 
-
-            player.keyReleased(e);
-
-        }
-
- 
-
-        @Override
-
-        public void keyPressed(KeyEvent e) {
-
- 
-
-            player.keyPressed(e);
-
- 
-
-            int x = player.getX();
-
-            int y = player.getY();
-
- 
-
-            int key = e.getKeyCode();
-
- 
-
-            if (key == KeyEvent.VK_SPACE) {
-
-
-                if (ingame) {
-
-                    if (!shot.isVisible()) {
-
-                        shot = new Shot(x, y);
-
-                    }
-
-                }
-
-            }
-
-        }
-
-    }
-
 }
-
-
-
