55c81808e4bb7da7ffc47cd0432fad3190bcf803
username2112 <36423664+username2112@users.noreply.github.com>
2019-11-04 14:23:09 -0800

    Add files via upload

---
+++ SpaceInvader/tests.java
@@ -0,0 +1,17 @@
+package SpaceInvader;
+
+import java.util.*;
+
+public class tests {
+
+	public static void main(String[] args) {
+		System.out.println("\\\\");
+		System.out.println("'");
+		System.out.println("\ta\tb\tc");
+		System.out.println("\"\"\"");
+		System.out.println("C:\nin\the downward spiral");
+		
+
+	}
+
+}
